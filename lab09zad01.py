b88b521b3927ba0f8631299b3246278805093385
Łukasz Perkowski <58466856+Rabajt@users.noreply.github.com>
2022-05-31 20:42:57 +0200

    Update lab09zad01.py

--- lab09zad01.py
+++ lab09zad01.py
@@ -12,3 +12,19 @@ secret_image.save("./1MB.jpg")
 secret_message = "to us."
 secret_image = lsbset.hide("./10MB.jpg", secret_message, generators.eratosthenes())
 secret_image.save("./10MB.jpg")
+
+"""
+from stegano import lsb
+
+secret = lsb.hide("./test/100KB.png", "All your")
+secret.save("./100KB.png")
+print(lsb.reveal("./100KB.png"))
+
+secret = lsb.hide("./test/1MB.png", "base are belong")
+secret.save("./1MB.png")
+print(lsb.reveal("./1MB.png"))
+
+secret = lsb.hide("./test/10MB.png", "to us.")
+secret.save("./10MB.png")
+print(lsb.reveal("./10MB.png"))
+"""
